824df0d6180585cd88bcf7b057d2eeb38de7be1e
a7654311 <37109501+a7654311@users.noreply.github.com>
2018-03-06 20:38:56 +0800

    Create pixadmove.js

---
+++ pixadmove.js
@@ -0,0 +1,1 @@
+var head=document.getElementsByTagName('head')[0];var link=document.createElement('link');link.rel='stylesheet';link.type='text/css';link.href='//blogbackup.000webhostapp.com/style/pixadmove.css';head.appendChild(link);
